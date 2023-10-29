62f4eba3e48e3c33019ad0c03c07b1f815f629f9
Arnold Twala <arnoldtwl@gmail.com>
2023-10-29 22:55:54 +0200

    update cors

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 
 // Add Middleware
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000', // Fallback to localhost if not set
   credentials: true,
   optionsSuccessStatus: 200
 };
