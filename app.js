727312a1ab2ab4bedad7875daf496dfb1871491d
Arnold Twala <arnoldtwl@gmail.com>
2023-10-29 22:45:26 +0200

    added trust proxy

--- app.js
+++ app.js
@@ -34,6 +34,9 @@ const corsOptions = {
   optionsSuccessStatus: 200
 };
 
+// Trust the proxy to get the real client IP
+app.set('trust proxy', 1);
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
